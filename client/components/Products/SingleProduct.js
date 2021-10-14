60edec476fb7e6af120f4f0514a77c2210228a86
Riviere Fougy <rivierefougy@gmail.com>
2021-10-14 10:43:51 -0400

    SingleProduct.js: changes made to code, now loads successfully on browser

--- client/components/Products/SingleProduct.js
+++ client/components/Products/SingleProduct.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-// ATTENTION: Make sure the path is correct.
 import { fetchSingleProduct } from "../../store/singleProduct";
 
 class SingleProduct extends Component {
@@ -16,29 +14,11 @@ class SingleProduct extends Component {
 
   render() {
     const singleProduct = this.props.product;
-    console.log(singleProduct);
     return (
       <div>
         <div id="singleProduct">
+          <h1>{singleProduct.name}</h1>
           <img src={singleProduct.imageURL} />
-          <h1>
-            {singleProduct.name}
-          </h1>
-          <h4>{singleProduct.email}</h4>
-          <h4>{singleProduct.gpa}</h4>
-        </div>
-        <div id="selectedStudentCampus">
-          <h2>Campus</h2>
-          {!singleProduct.campus ? (
-            <h1>The Selected Student does not attend a campus.</h1>
-          ) : (
-            <Link
-              to={`/campuses/${singleProduct.campus.id}`}
-              key={singleProduct.campus.id}
-            >
-              <h1>{singleProduct.campus.name}</h1>
-            </Link>
-          )}
         </div>
       </div>
     );
@@ -47,7 +27,7 @@ class SingleProduct extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    student: state.singleProduct,
+    product: state.singleProduct,
   };
 };
 
