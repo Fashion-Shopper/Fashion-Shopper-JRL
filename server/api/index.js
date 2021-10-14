24877161400d8ef77813fdd9270cfb79dd30c534
Riviere Fougy <rivierefougy@gmail.com>
2021-10-14 15:02:38 -0400

    server/api/index.js: added brandsRoute & implemented into code

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const usersRoute = require("./users");
 const productsRoute = require("./products");
+const brandsRoute = require("./brands");
 module.exports = router;
 
 router.use("/users", usersRoute);
 router.use("/products", productsRoute);
+router.use("/brands", brandsRoute);
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
