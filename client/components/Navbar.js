50e787d879d382ba9c3a5d9f0de62a2897f6f2a2
jmartin-code <jmmartinez331@gmail.com>
2021-10-13 22:51:07 -0400

    added cart to navbar when user is logged in

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 import { logout } from '../store'
 
 //Material UI Imports
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Badge, IconButton } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Navbar = () => {
   const dispatch = useDispatch()
@@ -15,28 +16,32 @@ const Navbar = () => {
   }
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Fashion-JRL
-          </Typography>
-          {isLoggedIn ? (
-            <>
-              <Button component={Link} to="/home" color="inherit">Home</Button>
-              <Button component={Link} to="/products" color="inherit">Products</Button>
-              <Button component={Link} to="/" onClick={handleClick} color="inherit">Logout</Button>
-            </>
-          ) : (
-            <>
-              <Button component={Link} to="/products" color="inherit">Products</Button>
-              <Button component={Link} to="/login" color="inherit">Login</Button>
-              <Button component={Link} to="/signup" color="inherit">Sign Up</Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Fashion-JRL
+        </Typography>
+        {isLoggedIn ? (
+          <>
+            <Button component={Link} to="/home" color="inherit">Home</Button>
+            <Button component={Link} to="/products" color="inherit">Products</Button>
+            <Button component={Link} to="/" onClick={handleClick} color="inherit">Logout</Button>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+
+          </>
+        ) : (
+          <>
+            <Button component={Link} to="/products" color="inherit">Products</Button>
+            <Button component={Link} to="/login" color="inherit">Login</Button>
+            <Button component={Link} to="/signup" color="inherit">Sign Up</Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
   )
 }
 
