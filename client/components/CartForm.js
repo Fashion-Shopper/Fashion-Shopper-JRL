6812966a7d1b70814a0b6dd42ff5623b6ee21528
Riviere Fougy <rivierefougy@gmail.com>
2021-10-16 10:11:26 -0400

    CartForm.js: Refactored boilerplate code

--- client/components/CartForm.js
+++ client/components/CartForm.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { createCampus } from "../store/campuses";
+// ATTENTION (Riv): Make sure to import thunk for adding orderItem to order state.
+// import { createCampus } from "../store/campuses";
 
-class CampusCreateForm extends Component {
+class CartForm extends Component {
   constructor() {
     super();
     this.state = {
-      name: "",
-      address: "",
+      quantity: 0,
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -19,44 +19,34 @@ class CampusCreateForm extends Component {
 
   onSubmit(evt) {
     evt.preventDefault();
-    const newCampus = {
-      name: this.state.name,
-      address: this.state.address,
+    const orderItem = {
+      quantity: this.state.quantity,
+      // ATTENTION (Riv): This is where we'll have the productId.
+      productId: this.state.productId,
     };
-    if (Object.values(newCampus).includes("")) {
+    if (Object.values(orderItem).includes(0)) {
       alert(`Please complete all required fields before submitting.`);
     } else {
-      this.props.create(newCampus);
-      this.setState({ name: "", address: "" });
+      this.props.create(orderItem);
+      this.setState({ quantity: 0 });
     }
   }
 
   render() {
     return (
-      <form className="campus-create-form">
+      <form className="cart-form">
         <div>
-          <label>Campus Name: </label>
+          <label>Order Product: </label>
           <br />
           <input
-            name="name"
+            name="quantity"
             type="text"
-            value={this.state.name}
+            value={this.state.quantity}
             onChange={this.onChange}
           />
-          <br />
-          <br />
-          <label>Campus Address: </label>
-          <br />
-          <input
-            name="address"
-            type="text"
-            value={this.state.address}
-            onChange={this.onChange}
-          />
-          <br />
           <br />
           <button type="submit" onClick={this.onSubmit}>
-            Add Campus
+            Add to Cart
           </button>
         </div>
       </form>
