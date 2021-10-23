c7c8afc2a82cde85100ea70631c166bb18fe640d
Riviere Fougy <rivierefougy@gmail.com>
2021-10-23 12:31:24 -0400

    components/Admin/Products: Added code to files and refactored

---
+++ client/components/Admin/Products/AdminProductsRow.js
@@ -0,0 +1,59 @@
+import React from "react";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import { Button, CardMedia } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function Row(props) {
+  const { id, imageURL, name, brandName, category, size, price } =
+    props.product;
+
+  return (
+    <>
+      <TableRow>
+        <TableCell scope="row" align="center">
+          {id}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          <CardMedia
+            component="img"
+            sx={{ maxWidth: 150, p: 3 }}
+            image={imageURL}
+            alt={name}
+          />{" "}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          {name}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          {brandName}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          {category}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          {size}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          {price}
+        </TableCell>
+        <TableCell scope="row" align="center">
+          <Button variant="outlined">Update</Button>
+        </TableCell>
+        <TableCell scope="row" align="center">
+          <Button
+            scope="row"
+            align="center"
+            onClick={() => handleDelete(id)}
+            variant="outlined"
+            startIcon={<DeleteIcon />}
+          >
+            Delete
+          </Button>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
+
+export default Row;
--- client/components/Admin/Products/AdminProductsTable.js
+++ client/components/Admin/Products/AdminProductsTable.js
@@ -1,18 +1,77 @@
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchAdminProducts } from "../../../store/admin/adminProducts";
+// import { ProductForm } from "../ProductForm";
+
+// const AdminProducts = () => {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(fetchAdminProducts());
+//   }, []);
+
+//   const adminproducts = useSelector((state) => state.adminProducts);
+
+//   return <pre>{JSON.stringify(adminproducts, null, 2)}</pre>;
+// };
+
+// export default AdminProducts;
+
+//////////////////////////////////////////////////////////////////
+
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAdminProducts } from "../../../store/admin/adminProducts";
-import { ProductForm } from "../ProductForm";
 
-const AdminProducts = () => {
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import LoadSpinner from "../../Materialui/LoadSpinner";
+import { Container } from "@mui/material";
+import Row from "./AdminProductsRow";
+
+const ProductsTable = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchAdminProducts());
   }, []);
 
-  const adminproducts = useSelector((state) => state.adminProducts);
+  const products = useSelector((state) => state.adminProducts);
 
-  return <pre>{JSON.stringify(adminproducts, null, 2)}</pre>;
+  if (!products) {
+    return <LoadSpinner />;
+  }
+
+  return (
+    <Container sx={{ mt: 3 }}>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center"> Id </TableCell>
+              <TableCell align="center">Image</TableCell>
+              <TableCell align="center">Title</TableCell>
+              <TableCell align="center">Brand</TableCell>
+              <TableCell align="center">Category</TableCell>
+              <TableCell align="center">Size</TableCell>
+              <TableCell align="center">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <Row key={product.id} product={product} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 };
 
-export default AdminProducts;
+export default ProductsTable;
