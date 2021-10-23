0b66d147b22e80a4cb878916182d0a5062ffab8b
Riviere Fougy <rivierefougy@gmail.com>
2021-10-23 11:21:14 -0400

    AdminUsersRow.js: removed unneeded code

--- client/components/Admin/Users/AdminUsersRow.js
+++ client/components/Admin/Users/AdminUsersRow.js
@@ -1,18 +1,6 @@
-import React, { useState } from "react";
-import dateFormatter from "dayjs";
-
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
+import React from "react";
 import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 function Row(props) {
   const { user } = props;
