2421051bcd2f6819803216ecdc8c212d57587a1e
Riviere Fougy <rivierefougy@gmail.com>
2021-10-25 15:04:26 -0400

    AdminProductsTable.js: removed commented code

--- client/components/Admin/Products/AdminProductsTable.js
+++ client/components/Admin/Products/AdminProductsTable.js
@@ -1,24 +1,3 @@
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchAdminProducts } from "../../../store/admin/adminProducts";
-// import { ProductForm } from "../ProductForm";
-
-// const AdminProducts = () => {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchAdminProducts());
-//   }, []);
-
-//   const adminproducts = useSelector((state) => state.adminProducts);
-
-//   return <pre>{JSON.stringify(adminproducts, null, 2)}</pre>;
-// };
-
-// export default AdminProducts;
-
-//////////////////////////////////////////////////////////////////
-
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../../store";
