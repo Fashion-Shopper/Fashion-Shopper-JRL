c6977e4e2d42c74722ec890bc8518b9b740f1479
Luna <flunawonder@gmail.com>
2021-10-29 08:07:24 -0400

    added NEW ARRIVALS to home page

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -3,13 +3,16 @@ import { Box } from '@mui/system'
 import React from 'react'
 import { } from 'react-redux'
 import SlideShow from './SlideShow'
+import NewArrivals from './NewArrivals'
 
 const Home = () => {
 
   return (
     <Box sx={{ mb: 15 }}>
       <SlideShow />
+      <NewArrivals />
     </Box>
+    
   )
 }
 
